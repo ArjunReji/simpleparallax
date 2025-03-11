2c5f227f37ac8504fa5eba5c7c5502a7eea2c431
ArjunReji <arjunreji364@gmail.com>
2025-03-12 01:33:06 +0530

    first commit

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("scroll", function() {
-  let scrollPosition = window.pageYOffset;
-  document.querySelectorAll(".parallax").forEach(parallax => {
-      parallax.style.backgroundPositionY = scrollPosition * 0.5 + "px";
-  });
-});
